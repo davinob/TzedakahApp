f94922cf7f32ee702754536f1da13161dd570efc
davinob <davinob@gmail.com>
2018-11-05 10:41:50 +0200

    shekel missed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -41,14 +41,14 @@ export class HomePage {
 
     this.chosenCountry=await  this.storage.get('chosenCountry');
       if (!this.chosenCountry)
-        this.chosenCountry={name:"israel", moneySign:"₪",matbeot:[0.1,0.5,2,5,10,20,50,100,200]};
+        this.chosenCountry={name:"israel", moneySign:"₪",matbeot:[0.1,0.5,1,2,5,10,20,50,100,200]};
   }
 
   countries=
   [
     {name:"us", moneySign:"$",  matbeot:[0.01,0.05,0.1,0.25,1,2,5,10,20,50,100]},
     {name:"eu", moneySign:"€",  matbeot:[0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50,100,200,500]},
-    {name:"israel", moneySign:"₪",  matbeot:[0.1,0.5,2,5,10,20,50,100,200]}
+    {name:"israel", moneySign:"₪",  matbeot:[0.1,0.5,1,2,5,10,20,50,100,200]}
   ];
 
   chooseCountry(country:string)
